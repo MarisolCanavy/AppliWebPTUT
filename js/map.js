beedd35d4fcfb93e7845acf0724091545688bf67
Marisol Cny <57805028+MarisolCanavy@users.noreply.github.com>
2020-04-01 16:26:43 +0200

    Afficher plusieurs marqueurs

--- js/map.js
+++ js/map.js
@@ -15,3 +15,31 @@ function positionnerMarqueur() {
     var pos = { lat: 43.215789, lng: 2.345170 };
     var marker = new google.maps.Marker({ position: pos, map: map });
 }
+
+var coordonnesV = [
+    [43.220934, 2.337686], //Patient1
+    [43.222685, 2.349477], //Patient2
+    [43.218146, 2.346757] //Patient3
+];
+
+var message = ['Patient 1', 'Patient 2', 'Patient 3'];
+
+function affichePlusieursMarqueurs() {
+    for (var i = 0; i < 3; ++i) {
+        var pos = { lat: coordonnesV[i][0], lng: coordonnesV[i][1] };
+        var marker = new google.maps.Marker({ position: pos, map: map });
+        attachMessage(marker, message[i]);
+    }
+}
+
+// Attache une info à un point
+function attachMessage(marker, message) {
+    var infowindow = new google.maps.InfoWindow({
+        content: message
+    });
+    //sur clic affiche le message
+    marker.addListener('click', function () {
+        infowindow.open(marker.get('map'), marker);
+    });
+}
+
