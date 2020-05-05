f9f321b9af6bfe863e25aa0855d1177d9d02dd10
EmmaSalvan <57804812+EmmaSalvan@users.noreply.github.com>
2020-05-05 11:10:16 +0200

    Add files via upload
    BDD icare

--- icare.sql
+++ icare.sql
@@ -1,36 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 avr. 2020 à 01:06
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
+-- Host: localhost:8889
+-- Generation Time: May 05, 2020 at 09:05 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `icare`
+-- Database: `icare`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `infirmier`
+-- Table structure for table `acteMedical`
 --
 
-DROP TABLE IF EXISTS `infirmier`;
-CREATE TABLE IF NOT EXISTS `infirmier` (
-  `idInfirmier` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `acteMedical` (
+  `idActePrevu` int(11) NOT NULL,
+  `début` datetime NOT NULL,
+  `fin` datetime NOT NULL,
+  `durée` int(11) NOT NULL,
+  `nbIntervenant` int(11) NOT NULL DEFAULT '1',
+  `commentaire` text NOT NULL,
+  `idTypeActe_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `infirmier`
+--
+
+CREATE TABLE `infirmier` (
+  `idInfirmier` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `email` text NOT NULL,
@@ -39,12 +46,11 @@ CREATE TABLE IF NOT EXISTS `infirmier` (
   `codePostal` int(11) NOT NULL,
   `ville` varchar(50) NOT NULL,
   `telephone` bigint(20) NOT NULL,
-  `commentaire` text NOT NULL,
-  PRIMARY KEY (`idInfirmier`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `commentaire` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `infirmier`
+-- Dumping data for table `infirmier`
 --
 
 INSERT INTO `infirmier` (`idInfirmier`, `nom`, `prenom`, `email`, `dateNaissance`, `adresse`, `codePostal`, `ville`, `telephone`, `commentaire`) VALUES
@@ -54,12 +60,22 @@ INSERT INTO `infirmier` (`idInfirmier`, `nom`, `prenom`, `email`, `dateNaissance
 -- --------------------------------------------------------
 
 --
--- Structure de la table `patient`
+-- Table structure for table `motifPriseEnCharge`
 --
 
-DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
-  `idPatient` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `motifPriseEnCharge` (
+  `idMotif` int(11) NOT NULL,
+  `intitule` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `idPatient` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `email` text NOT NULL,
@@ -71,12 +87,11 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `commentaire` text NOT NULL,
   `taille` int(11) NOT NULL,
   `poids` int(11) NOT NULL,
-  `typeHabitation` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`idPatient`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `typeHabitation` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `patient`
+-- Dumping data for table `patient`
 --
 
 INSERT INTO `patient` (`idPatient`, `nom`, `prenom`, `email`, `dateNaissance`, `adresse`, `codePostal`, `ville`, `telephone`, `commentaire`, `taille`, `poids`, `typeHabitation`) VALUES
@@ -85,31 +100,155 @@ INSERT INTO `patient` (`idPatient`, `nom`, `prenom`, `email`, `dateNaissance`, `
 -- --------------------------------------------------------
 
 --
--- Structure de la table `profil`
+-- Table structure for table `priseEnCharge`
 --
 
-DROP TABLE IF EXISTS `profil`;
-CREATE TABLE IF NOT EXISTS `profil` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `priseEnCharge` (
+  `idPrise` int(11) NOT NULL,
+  `idPatient_fk` int(11) NOT NULL,
+  `idInfirmier_fk` int(11) NOT NULL,
+  `dateP` date NOT NULL,
+  `commentaire` text NOT NULL,
+  `idMotif_fk` int(11) NOT NULL,
+  `idActePrevu_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profil`
+--
+
+CREATE TABLE `profil` (
+  `id` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `email` text NOT NULL,
-  `password` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `profil`
+-- Dumping data for table `profil`
 --
 
 INSERT INTO `profil` (`id`, `nom`, `prenom`, `email`, `password`) VALUES
-(23, 'Goetschel', 'Numa', 'numa@mail.com', '$2y$10$sCTv7Wmc998To3CFm6tbZuPScImbPoX/bjyUZbT1b6FOjF.7GuqEW'),
 (5, 'test', 'test', 'test@test.com', '$2y$10$dvh3HAKgk4Xj429/63Qa6eingZf1si2cLGV3zDcMiSgfoy2mRYVRS'),
+(23, 'Goetschel', 'Numa', 'numa@mail.com', '$2y$10$sCTv7Wmc998To3CFm6tbZuPScImbPoX/bjyUZbT1b6FOjF.7GuqEW'),
 (24, 'testnu', 'ma', 'te@te.te', '$2y$10$bhWquGzR7Ckm2mwvnwT.QOxYLNDtU0ccwUgSYNx2OUaqGB0DuAmmG'),
 (25, 'lala', 'la', 'la@la.la', '$2y$10$49aUlueh6W1laQoh5cHGxe8dsSeCkgAqqCJuSr5U.cWProO/Nsyvi'),
 (26, 'Marisol', 'Marisol', 'ma@am.com', '$2y$10$Y6.jJZgZPe5Jjz3LYJAsa.FeJoEZxVpuhU.iI/wgQ1LLPLlh9xOsC');
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ressourceMat`
+--
+
+CREATE TABLE `ressourceMat` (
+  `typeVehicule` varchar(50) NOT NULL,
+  `consoEssence` int(11) NOT NULL,
+  `materiel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typeActe`
+--
+
+CREATE TABLE `typeActe` (
+  `idActe` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `acteMedical`
+--
+ALTER TABLE `acteMedical`
+  ADD PRIMARY KEY (`idActePrevu`),
+  ADD KEY `idTypeActe_fk` (`idTypeActe_fk`);
+
+--
+-- Indexes for table `infirmier`
+--
+ALTER TABLE `infirmier`
+  ADD PRIMARY KEY (`idInfirmier`);
+
+--
+-- Indexes for table `motifPriseEnCharge`
+--
+ALTER TABLE `motifPriseEnCharge`
+  ADD PRIMARY KEY (`idMotif`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`idPatient`);
+
+--
+-- Indexes for table `priseEnCharge`
+--
+ALTER TABLE `priseEnCharge`
+  ADD PRIMARY KEY (`idPrise`),
+  ADD KEY `idInfirmier_fk` (`idInfirmier_fk`),
+  ADD KEY `idPatient_fk` (`idPatient_fk`),
+  ADD KEY `idMotif_fk` (`idMotif_fk`),
+  ADD KEY `idActePrevu_fk` (`idActePrevu_fk`);
+
+--
+-- Indexes for table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `typeActe`
+--
+ALTER TABLE `typeActe`
+  ADD PRIMARY KEY (`idActe`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `infirmier`
+--
+ALTER TABLE `infirmier`
+  MODIFY `idInfirmier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `idPatient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `acteMedical`
+--
+ALTER TABLE `acteMedical`
+  ADD CONSTRAINT `idTypeActe_fk` FOREIGN KEY (`idTypeActe_fk`) REFERENCES `typeActe` (`idActe`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `priseEnCharge`
+--
+ALTER TABLE `priseEnCharge`
+  ADD CONSTRAINT `idActePrevu_fk` FOREIGN KEY (`idActePrevu_fk`) REFERENCES `acteMedical` (`idActePrevu`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `idInfirmier_fk` FOREIGN KEY (`idInfirmier_fk`) REFERENCES `infirmier` (`idInfirmier`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `idMotif_fk` FOREIGN KEY (`idMotif_fk`) REFERENCES `motifPriseEnCharge` (`idMotif`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `idPatient_fk` FOREIGN KEY (`idPatient_fk`) REFERENCES `patient` (`idPatient`) ON UPDATE CASCADE;
