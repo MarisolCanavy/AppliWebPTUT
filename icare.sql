f53917c962a7970425518a0997a82929537bf736
EmmaSalvan <57804812+EmmaSalvan@users.noreply.github.com>
2020-05-10 17:57:17 +0200

    Modifications BDD
    icare.sql

--- icare.sql
+++ icare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 05, 2020 at 09:05 AM
+-- Generation Time: May 10, 2020 at 03:48 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -22,13 +22,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `acteMedical` (
   `idActePrevu` int(11) NOT NULL,
-  `début` datetime NOT NULL,
-  `fin` datetime NOT NULL,
   `durée` int(11) NOT NULL,
   `nbIntervenant` int(11) NOT NULL DEFAULT '1',
   `commentaire` text NOT NULL,
   `idTypeActe_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `acteMedical`
+--
+
+INSERT INTO `acteMedical` (`idActePrevu`, `durée`, `nbIntervenant`, `commentaire`, `idTypeActe_fk`) VALUES
+(1, 2, 1, 'Cet acte est la suite d\'une prise du taux de glycémie.', 1),
+(2, 15, 1, 'Désinfection de la plaie\r\nChangement du pansement', 14),
+(3, 30, 1, '', 22);
 
 -- --------------------------------------------------------
 
@@ -54,8 +61,10 @@ CREATE TABLE `infirmier` (
 --
 
 INSERT INTO `infirmier` (`idInfirmier`, `nom`, `prenom`, `email`, `dateNaissance`, `adresse`, `codePostal`, `ville`, `telephone`, `commentaire`) VALUES
-(1, 'Goetschel', 'Numa', 'mezafklpo@zefl.pol', '1999-04-11', '52ju', 81200, 'mzt', 3651560, 'Julfan'),
-(4, 'test', 'a', 'm@m.m', '2020-04-11', '52ju', 1, 'mzt', 23, 'gfhjik');
+(1, 'Muller', 'Lisa', 'lisa.muller@hotmail.fr', '1978-09-22', '12 rue Bellevue', 81100, 'Castres', 639769905, 'RAS'),
+(3, 'BERTRAND', 'Louis', 'louis.bertrand@gmail.com', '1980-01-25', '5 place de La Croix', 81100, 'Castres', 690342965, 'RAS'),
+(6, 'ROUSSEAU', 'Maria', 'maria.rousseau@free.fr', '1990-06-18', '18 chemin de Côte Rousse', 81100, 'Castres', 653893957, 'RAS'),
+(7, 'GUERRIN', 'Isabelle', 'isa.guerrin@orange.fr', '1986-03-20', '3 rue des pins', 81100, 'Burlats', 651809214, 'RAS');
 
 -- --------------------------------------------------------
 
@@ -67,6 +76,17 @@ CREATE TABLE `motifPriseEnCharge` (
   `idMotif` int(11) NOT NULL,
   `intitule` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `motifPriseEnCharge`
+--
+
+INSERT INTO `motifPriseEnCharge` (`idMotif`, `intitule`) VALUES
+(1, 'Grabataire '),
+(2, 'Post-opératoire'),
+(3, 'Chimiothérapie'),
+(4, 'Ostéoporose'),
+(5, 'Diabète');
 
 -- --------------------------------------------------------
 
@@ -95,7 +115,10 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`idPatient`, `nom`, `prenom`, `email`, `dateNaissance`, `adresse`, `codePostal`, `ville`, `telephone`, `commentaire`, `taille`, `poids`, `typeHabitation`) VALUES
-(1, 'test', 'Numa', 'dza@tr.l', '2020-04-22', '52ju', 1, 'mzt', 365156010, 'uhy', 147, 57, 'state1');
+(1, 'ROUX', 'Monique', 'roux.monique@free.fr', '2020-10-30', '20 rue Canteloube', 81100, 'Castres', 639821812, 'Ostéoporose', 160, 65, 'Maison'),
+(2, 'DURAND', 'Charles', 'charles.durand@gmail.com', '1940-05-06', '3 chemin de la barque', 81100, 'Castres', 658392019, 'Diabétique de type 2', 174, 90, 'Maison'),
+(5, 'MARTIN', 'Rose', 'rose.martin@gmail.com', '1933-03-17', '9 rue des jardins neufs', 81100, 'Castres', 640398621, 'Hospitalisée pour chute le 12/02/2000\r\n', 155, 56, 'Appartement'),
+(9, 'PETIT', 'Patrick', 'patrick.petit@gmail.com', '1930-08-06', '15 rue de l\'arc boisé', 81100, 'Castres', 670806521, 'Grabataire', 176, 85, 'Maison');
 
 -- --------------------------------------------------------
 
@@ -134,7 +157,6 @@ CREATE TABLE `profil` (
 INSERT INTO `profil` (`id`, `nom`, `prenom`, `email`, `password`) VALUES
 (5, 'test', 'test', 'test@test.com', '$2y$10$dvh3HAKgk4Xj429/63Qa6eingZf1si2cLGV3zDcMiSgfoy2mRYVRS'),
 (23, 'Goetschel', 'Numa', 'numa@mail.com', '$2y$10$sCTv7Wmc998To3CFm6tbZuPScImbPoX/bjyUZbT1b6FOjF.7GuqEW'),
-(24, 'testnu', 'ma', 'te@te.te', '$2y$10$bhWquGzR7Ckm2mwvnwT.QOxYLNDtU0ccwUgSYNx2OUaqGB0DuAmmG'),
 (25, 'lala', 'la', 'la@la.la', '$2y$10$49aUlueh6W1laQoh5cHGxe8dsSeCkgAqqCJuSr5U.cWProO/Nsyvi'),
 (26, 'Marisol', 'Marisol', 'ma@am.com', '$2y$10$Y6.jJZgZPe5Jjz3LYJAsa.FeJoEZxVpuhU.iI/wgQ1LLPLlh9xOsC');
 
@@ -160,6 +182,24 @@ CREATE TABLE `typeActe` (
   `idActe` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `typeActe`
+--
+
+INSERT INTO `typeActe` (`idActe`, `nom`) VALUES
+(1, 'Injection insuline'),
+(2, 'Injection Prolia ostéoporose'),
+(7, 'Traitement chimiothérapie'),
+(9, 'Vérification taux insuline'),
+(14, 'Pansement'),
+(15, 'Prise tension'),
+(17, 'Pose sonde vésicale'),
+(18, 'Pesée'),
+(20, 'Distribution médicament'),
+(22, 'Toilette'),
+(25, 'Habillage'),
+(28, 'Changement couche');
 
 --
 -- Indexes for dumped tables
@@ -220,13 +260,13 @@ ALTER TABLE `typeActe`
 -- AUTO_INCREMENT for table `infirmier`
 --
 ALTER TABLE `infirmier`
-  MODIFY `idInfirmier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idInfirmier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `idPatient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idPatient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `profil`
